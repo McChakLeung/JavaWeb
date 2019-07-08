485bc7bc90988b93715416f9c0a75e658d9cf0f9
McChakLeung <441337516@qq.com>
2019-07-08 17:45:07 +0800

    JavaWeb项目--第一个Servlet

---
+++ ServletTest/src/main/java/com/dgpalife/servlet/IndexServlet.java
@@ -0,0 +1,16 @@
+package com.dgpalife.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class IndexServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        this.doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().write("hello");
+    }
+}
