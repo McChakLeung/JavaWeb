4bebbcb93fe1101a4ac9ec94eda2f799ff1f5507
McChakLeung <441337516@qq.com>
2019-08-19 23:02:49 +0800

    Glyphicon类逆向工程

---
+++ Generator/src/main/java/com/atguigu/atcrowdfunding/bean/Glyphicon.java
@@ -0,0 +1,24 @@
+package com.atguigu.atcrowdfunding.bean;
+
+public class Glyphicon {
+
+    private Integer id;
+
+    private String icon_name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIcon_name() {
+        return icon_name;
+    }
+
+    public void setIcon_name(String icon_name) {
+        this.icon_name = icon_name;
+    }
+}
